a4802c3ee81293d60ae0fdcd105b14c8f7bb5826
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-03 21:35:02 -0700

    :new: Add getRange helper

--- src/helpers.js
+++ src/helpers.js
@@ -5,6 +5,7 @@
 import NativeFS from 'fs'
 import request from 'request'
 import promisify from 'sb-promisify'
+import type {PoolWorker} from 'range-pool'
 
 export function promisedRequest(options: Object): Promise{
   return new Promise(function(resolve, reject) {
@@ -13,6 +14,17 @@ export function promisedRequest(options: Object): Promise{
     job.on('response', resolve)
     job.pause()
   })
+}
+
+export function getRange(worker: PoolWorker): ?string {
+  let range = null
+  if (worker.getCurrentIndex() !== 0) {
+    range = worker.getCurrentIndex() + '-'
+    if (worker.getIndexLimit() !== Infinity) {
+      range += worker.getIndexLimit()
+    }
+  }
+  return range ? 'bytes=' + range : range
 }
 
 export const FS = {
