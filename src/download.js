d8e944a4aca6ac6e73cdda70f555971f9f355620
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-03 21:35:30 -0700

    :new: Implement getConnection and start in new download class

--- src/download.js
+++ src/download.js
@@ -2,7 +2,11 @@
 
 /* @flow */
 
+import Path from 'path'
 import {Emitter, CompositeDisposable} from 'sb-event-kit'
+import {RangePool} from 'range-pool'
+import {Connection} from './connection'
+import {FS} from './helpers'
 import type {Disposable} from 'sb-event-kit'
 import type {Downloader$Job} from './types'
 
@@ -10,11 +14,38 @@ export class Download {
   options: Downloader$Job;
   subscriptions: CompositeDisposable;
   emitter: Emitter;
+  connections: Set<Connection>;
+  pool: RangePool;
 
   constructor(options: Downloader$Job) {
     this.subscriptions = new CompositeDisposable()
     this.emitter = new Emitter()
+    this.connections = new Set()
+    this.pool = new RangePool(1)
     this.options = options
+  }
+  async start(): Promise {
+    const connection = await this.getConnection(0)
+    const fileInfo = {
+      path: Path.join(this.options.target.directory, this.options.target.file || connection.getFileName()),
+      size: connection.getFileSize()
+    }
+    const fd = FS.open(fileInfo.path, 'w')
+
+    this.pool.limit = fileInfo.size
+    connection.worker.limitIndex = fileInfo.size
+
+    const promises = []
+    for (let i = 1; i < this.options.connections; ++i) {
+      promises.push(this.getConnection(i))
+    }
+
+    await Promise.all(promises)
+
+    this.emitter.emit('did-start', {fileSize: fileInfo.path, filePath: fileInfo.path, url: this.options.url})
+    for (const connection of this.connections) {
+      connection.start(fd)
+    }
   }
   onDidError(callback: ((error: Error) => void)): Disposable {
     return this.emitter.on('did-error', callback)
@@ -30,5 +61,11 @@ export class Download {
   }
   dispose() {
     this.subscriptions.dispose()
+  }
+  async getConnection(index: number): Promise<Connection> {
+    const connection = new Connection(this.options.url, this.pool)
+    this.connections.add(connection)
+    await connection.activate()
+    return connection
   }
 }
