713ce9e52182a79b61b6327f32b6c629091cc98c
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-03 22:00:49 -0700

    :fire: Remove debug code

--- src/index.js
+++ src/index.js
@@ -5,22 +5,6 @@
 import {Download} from './download'
 import type {Downloader$Job} from './types'
 
-export async function download(options: Downloader$Job): Promise<Download> {
-  // TODO: Do all kind of validation here
+export function download(options: Downloader$Job): Download {
   return new Download(options)
 }
-process.on('uncaughtError', function(e) {
-  console.log(e.stack || e)
-})
-process.on('uncaughtPromiseRejection', function(e) {
-  console.log(e.stack || e)
-})
-download({
-  url: 'http://thewebsmith.co/blog/wp-content/uploads/song',
-  target: {
-    directory: '/tmp'
-  },
-  connections: 5
-}).then(function(d) {
-  d.download().catch(e => console.log(e.stack || e))
-}).catch(e => console.log(e.stack || e))
